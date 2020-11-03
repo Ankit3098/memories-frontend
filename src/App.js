c7ff8c03c39a49db8a0d4ec546cd43d022ce34a6
Ankit3098 <ankitpatel3098@gmail.com>
2020-11-03 21:54:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useState} from 'react'
+import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core'
+import {useDispatch} from 'react-redux'
+import {getPosts} from './actions/actionPosts'
+import memories from './images/memories.png'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import './app.css'
 
 function App() {
+  const [currentId, setCurrentId] = useState(0)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(getPosts())
+  }, [currentId, dispatch])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container maxWidth='lg'>
+        <AppBar className="appBar" position='static' color='inherit'>
+          <Typography className="heading" variant='h2' align='center'>Memories</Typography>
+          <img className="image" src={memories} alt='memories' height='60' />
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify='space-between' spacing={3} alignItems='stretch'>
+              <Grid item xs={12} sm={7}>
+                <Posts setCurrentId={setCurrentId} />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form currentId={currentId} setCurrentId={setCurrentId} />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
     </div>
   );
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+  
+import { combineReducers } from 'redux';
+import posts from './reducerPosts'
+export default combineReducers({
+    posts
+})
